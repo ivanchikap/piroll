138bda002613b73d147a4b646d7440dfa086a29b
ivanchikap <anton.ivanchik@gmail.com>
2018-12-07 12:34:27 +0200

    edit gitignore and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,36 @@ sass.compiler = require('node-sass');
 
 gulp.task('styles', function () {
     return gulp.src('src/css/*.scss')
-        // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        // .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
+        .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('css', function() {
+    gulp.src('src/css/**/*.css')
         .pipe(gulp.dest('build/css/'));
 });
 
 gulp.task('views', function() {
     gulp.src('src/*.html')
         .pipe(gulp.dest('build/'));
-})
+});
 
 gulp.task('img', function() {
-    gulp.src('src/img/*.*')
+    gulp.src('src/img/**/*.*')
         .pipe(gulp.dest('build/img/'));
-})
+});
 
 gulp.task('scripts', function() {
     gulp.src('src/js/*.js')
         .pipe(gulp.dest('build/js/'));
-})
+});
 
-gulp.task('build', ['styles', 'views', 'img', 'scripts']);
+gulp.task('build', ['styles', 'css', 'views', 'img', 'scripts']);
 
 gulp.task('watch', ['build'], function() {
-    gulp.watch('src/css/*.scss', ['styles']);
+    gulp.watch('src/css/**/*.scss', ['styles']);
     gulp.watch('src/*.html', ['views']);
     gulp.watch('src/img/*.*', ['img']);
     gulp.watch('src/js/*.js', ['scripts']);
