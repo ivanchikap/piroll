74db532fa5962f4649a34e01c1e60b0e935b2739
ivanchikap <anton.ivanchik@gmail.com>
2018-12-07 12:55:41 +0200

    nothing special

---
+++ src/js/init.js
@@ -0,0 +1,24 @@
+// $(function() {
+//     $('.release-products__tabs').tabs();
+//
+//     (function () {
+//         // .menu__nav -  навигация
+//         let nav = $('.menu__nav');
+//
+//         // .menu-btn - кнопка меню
+//         $('.menu__btn').click(function() {
+//             if(nav.hasClass('menu__nav--active')) {
+//                 nav.removeClass('menu__nav--active');
+//             }
+//             else {
+//                 nav.addClass('menu__nav--active');
+//             }
+//         });
+//     }());
+//
+//     $('.trending-products__carousel').owlCarousel({
+//         loop:true,
+//         dots: true,
+//         items:1
+//     });
+// });
