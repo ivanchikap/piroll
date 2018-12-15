2ef26df0596d3e936e41bdc5cd39c79757d6f444
ivanchikap <anton.ivanchik@gmail.com>
2018-12-15 11:53:28 +0200

    job at adaptive menu

--- src/js/init.js
+++ src/js/init.js
@@ -1,24 +1,43 @@
-// $(function() {
-//     $('.release-products__tabs').tabs();
-//
-//     (function () {
-//         // .menu__nav -  навигация
-//         let nav = $('.menu__nav');
-//
-//         // .menu-btn - кнопка меню
-//         $('.menu__btn').click(function() {
-//             if(nav.hasClass('menu__nav--active')) {
-//                 nav.removeClass('menu__nav--active');
-//             }
-//             else {
-//                 nav.addClass('menu__nav--active');
-//             }
-//         });
-//     }());
-//
-//     $('.trending-products__carousel').owlCarousel({
-//         loop:true,
-//         dots: true,
-//         items:1
-//     });
-// });
+$(function() {
+    // $('.release-products__tabs').tabs();
+     (function () {
+         // .menu__nav -  навигация
+         let nav = $('.header__nav');
+
+         // .menu-btn - кнопка меню
+         $('.header__btn').click(function() {
+             if(nav.hasClass('header__nav--active')) {
+                 nav.removeClass('header__nav--active');
+             }
+             else {
+                 nav.addClass('header__nav--active');
+             }
+         });
+     }());
+
+
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     dots: true,
+    //     // margin: 15,
+    //     items: 4,
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             dots: false
+    //         },
+    //         620: {
+    //             items: 2
+    //         },
+    //         950: {
+    //             items: 3
+    //         },
+    //         1370: {
+    //             items: 4
+    //         },
+    //     }
+    // });
+    //
+    // $('.upload-form select').styler();
+    // $('.upload-form .upload-form__type-file').styler();
+});
