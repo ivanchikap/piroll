4cffce329559adb6f04ba2e8e355322e92eaf490
ivanchikap <anton.ivanchik@gmail.com>
2018-12-21 21:41:59 +0200

    add sprite generetor, gulfile and packegs.json edited

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-'use strict';
+'use strict'
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
+var spritesmith = require('gulp.spritesmith');
 sass.compiler = require('node-sass');
 
 
@@ -19,24 +20,33 @@ gulp.task('styles', function () {
 gulp.task('css', function() {
     gulp.src('src/css/**/*.css')
         .pipe(gulp.dest('build/css/'));
-});
+})
 
 gulp.task('views', function() {
     gulp.src('src/*.html')
         .pipe(gulp.dest('build/'))
         .pipe(browserSync.reload({stream: true}));
-});
+})
 
 gulp.task('img', function() {
     gulp.src('src/img/**/*.*')
         .pipe(gulp.dest('build/img/'));
-});
+})
+
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('src/sprite/*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css',
+        padding: 3
+    }));
+    return spriteData.pipe(gulp.dest('build/sprite'));
+})
 
 gulp.task('scripts', function() {
     gulp.src('src/js/*.js')
         .pipe(gulp.dest('build/js/'))
         .pipe(browserSync.reload({stream: true}));
-});
+})
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -46,12 +56,13 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('build', ['styles', 'css', 'views', 'img', 'scripts']);
+gulp.task('build', ['styles', 'css', 'views', 'img', 'sprite', 'scripts']);
 
 gulp.task('watch', ['build'], function() {
     gulp.watch('src/css/**/*.scss', ['styles']);
     gulp.watch('src/*.html', ['views']);
     gulp.watch('src/img/*.*', ['img']);
     gulp.watch('src/js/*.js', ['scripts']);
+
     gulp.start('browser-sync');
 });
